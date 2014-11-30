e7b1f1183fdd5e08e3a2a427ac38efd66c113ab1
meew0 <blactbt@live.de>
2014-11-30 19:24:54 +0100

    Fix bug in talk module

--- modules/talk.rb
+++ modules/talk.rb
@@ -22,16 +22,16 @@ farewell_regex = /(good)?b(ye|ai)|cya/i
 identity_regex = /who (is|are)( you)?/i
 
 # Now check if one of the regexes is found in the message.
-if msg =~ greeting_regex
+if (msg =~ greeting_regex) == 0
   responses = ["What's up?", "How are you today?", "How goes?", "Nice to see you!"]
   puts "Hello #{user}! #{responses.sample}"
 end
 
-if msg =~ farewell_regex
+if (msg =~ farewell_regex) == 0
   responses = ["Hope to see you again!", "See you soon!", "See ya!"]
   puts "Bye #{user} :( #{responses.sample}"
 end
 
-if msg =~ identity_regex
+if (msg =~ identity_regex) == 0
   puts "Hey there #{user}! I'm a bot programmed by meew0 to do various tasks!"
 end
