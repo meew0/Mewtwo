1d97bde32ab7a580b6ba5705df1a6878bd05393f
meew0 <blactbt@live.de>
2015-04-01 16:49:01 +0200

    Get rid of all other classes

--- src/main/java/meew0/mewtwo/MewtwoMain.java
+++ src/main/java/meew0/mewtwo/MewtwoMain.java
@@ -1,5 +1,7 @@
 package meew0.mewtwo;
 
+import meew0.mewtwo.thread.IRCBot;
+
 public class MewtwoMain {
 
     /**
@@ -7,5 +9,7 @@ public class MewtwoMain {
      * @param args The command line arguments, unused
      */
 	public static void main(String[] args) {
+        IRCBot bot = new IRCBot("127.0.0.1", 6667, "Mewtwo");
+        bot.start();
 	}
 }
