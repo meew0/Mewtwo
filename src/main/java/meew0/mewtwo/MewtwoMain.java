725f1e142ea0b316ff9e0f426ca326b87d71dd87
meew0 <blactbt@live.de>
2015-04-01 15:56:54 +0200

    Get rid of exception signature in main method

--- src/main/java/meew0/mewtwo/MewtwoMain.java
+++ src/main/java/meew0/mewtwo/MewtwoMain.java
@@ -1,21 +1,16 @@
 package meew0.mewtwo;
 
-import org.apache.commons.configuration.ConfigurationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-
-@SuppressWarnings("WeakerAccess")
 public class MewtwoMain {
     public static Logger mewtwoLogger;
 
     /**
      * Main method, shouldn't be called
      * @param args The command line arguments, unused
-     * @throws ConfigurationException in case something happens while loading configs
      */
-	public static void main(String[] args) throws ConfigurationException, IOException {
+	public static void main(String[] args) {
         // Make logger
         mewtwoLogger = LoggerFactory.getLogger("Mewtwo");
 	}
