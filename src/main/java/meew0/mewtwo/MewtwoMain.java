209859937c8ee183ee7ae3b5f73b06f6c77ce5d0
meew0 <blactbt@live.de>
2015-04-01 16:46:57 +0200

    Get rid of logger

--- src/main/java/meew0/mewtwo/MewtwoMain.java
+++ src/main/java/meew0/mewtwo/MewtwoMain.java
@@ -1,17 +1,11 @@
 package meew0.mewtwo;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class MewtwoMain {
-    public static Logger mewtwoLogger;
 
     /**
      * Main method, shouldn't be called
      * @param args The command line arguments, unused
      */
 	public static void main(String[] args) {
-        // Make logger
-        mewtwoLogger = LoggerFactory.getLogger("Mewtwo");
 	}
 }
