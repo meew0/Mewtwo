faaa4241b37219d3adb188e65c7403ad3a5bbe1b
meew0 <blactbt@live.de>
2015-04-02 20:20:55 +0200

    Add User class as a replacement for PircBotX's User

---
+++ src/main/java/meew0/mewtwo/irc/User.java
@@ -0,0 +1,34 @@
+package meew0.mewtwo.irc;
+
+/**
+ * Created by meew0 on 02.04.15.
+ */
+public class User {
+    private final String nick;
+    private final String fullHostmask;
+    private final String hostmask;
+    private final IRCBot bot;
+
+    public User(String nick, String fullHostmask, String hostmask, IRCBot bot) {
+        this.nick = nick;
+        this.fullHostmask = fullHostmask;
+        this.hostmask = hostmask;
+        this.bot = bot;
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public String getFullHostmask() {
+        return fullHostmask;
+    }
+
+    public String getHostmask() {
+        return hostmask;
+    }
+
+    public IRCBot getBot() {
+        return bot;
+    }
+}
