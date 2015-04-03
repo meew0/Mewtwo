fa69ca3e98c7829bb885dc3eb73c41c8ddab3028
meew0 <blactbt@live.de>
2015-04-03 12:05:12 +0200

    Re-add MewtwoMain.getConfig

--- src/main/java/meew0/mewtwo/MewtwoMain.java
+++ src/main/java/meew0/mewtwo/MewtwoMain.java
@@ -2,9 +2,28 @@ package meew0.mewtwo;
 
 import meew0.mewtwo.core.ShutdownHook;
 import meew0.mewtwo.irc.IRCBot;
+import org.apache.commons.configuration.HierarchicalINIConfiguration;
 
 public class MewtwoMain {
     public static String prefix = "%";
+
+    /**
+     * Returns a config file, creating it if it doesn't exist
+     * @param configName The path to the file
+     * @return a config file
+     */
+    public static HierarchicalINIConfiguration getConfig(String configName) {
+        try {
+            File cfgFile = new File(configName);
+            if (!cfgFile.exists() && !cfgFile.createNewFile())
+                mewtwoLogger.error("Could not create config file " + configName);
+            return new HierarchicalINIConfiguration(configName);
+        } catch(Throwable t) {
+            mewtwoLogger.error("Error while creating config file " + configName + "!", t);
+        }
+
+        return null;
+    }
 
     /**
      * Main method, shouldn't be called
